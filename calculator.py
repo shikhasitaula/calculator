d91d7f12e7f0d90e6a12dcb3cbeb3aaa898ca06d
shikhasitaula <129104368+shikhasitaula@users.noreply.github.com>
2023-03-27 17:04:47 -0700

    Add division method

--- calculator.py
+++ calculator.py
@@ -7,7 +7,11 @@ def subtract(x, y):
 def multipliy(x, y):
     return x * y
 
+def division(x , y):
+    return(x / y)
+
 print(add(2, 3))
 print(subtract(2,3))
 print(multipliy(2,3))
+print(divide(2, 3))
 
